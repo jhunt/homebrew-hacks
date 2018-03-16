948bfe309ea6b25f931871cedc29ddbcaa691f68
James Hunt <james@niftylogic.com>
2018-03-16 10:54:03 -0400

    Fix description of tt.  It's no-nonsense.

--- Formula/tt.rb
+++ Formula/tt.rb
@@ -2,7 +2,7 @@ class Tt < Formula
   s = "679d7387bddd88dd1613186b25f0f1f47bb11be3a7977aad2a9bb81966cb9a52" # ci:sha256
   v = "1.0" # ci:version
 
-  desc     "A non-nonsense time tracker"
+  desc     "A no-nonsense time tracker"
   homepage "https://jameshunt.us/code/tt"
 
   url      "https://github.com/jhunt/tt/archive/v#{v}.tar.gz"
