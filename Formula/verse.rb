f75628e663b12646be8b70c8ba3e177077bf5410
James Hunt <james@niftylogic.com>
2018-07-09 15:32:21 -0400

    Fix checksum for Verse 0.10.0

--- Formula/verse.rb
+++ Formula/verse.rb
@@ -1,5 +1,5 @@
 class Verse < Formula
-  s = "0c63640cb0b14eddf58bab19c7cecf141a36ad9d29ca55a0da9de0919342f678" # ci:sha256
+  s = "cad0df12692905395116865d986f2a179f91d905cb986f6f6f15337d46cbf8dc" # ci:sha256
   v = "0.10.0" # ci:version
 
   desc     "A Static Site Generator"
