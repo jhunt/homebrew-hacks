438bb75d0e34c2922bd027a781e272f8a9a84109
James Hunt <james@niftylogic.com>
2018-11-05 22:26:12 -0500

    Update osb to 0.1.1

--- Formula/osb.rb
+++ Formula/osb.rb
@@ -1,8 +1,8 @@
 class Osb < Formula
-  s = "043c0f41681af6b11b81626cb56af3ff89e37bb910a3ec8d8357ead695d9c2c2" # ci:sha256
-  v = "0.1.0" # ci:version
+  s = "691054a8f2b0b082964f99b3c4ed7477da73e5e2bfd7911f349ac22c07753718" # ci:sha256
+  v = "0.1.1" # ci:version
 
-  desc     "An A CLI for interacting with OSBAPI-compliant Service Brokers"
+  desc     "A CLI for interacting with OSBAPI-compliant Service Brokers"
   homepage "https://jameshunt.us/code/osb"
 
   url      "https://github.com/jhunt/osb/releases/download/v#{v}/osb-darwin-amd64"
